19beb1cf2899790a196a951a07c626caee5d23fb
Mihály Nyeste <mihaly.nyeste@gmail.com>
2013-10-18 00:18:27 +0200

    Read exif data

---
+++ lib/read-exif.rb
@@ -0,0 +1,19 @@
+require 'rubygems' 
+require 'bundler/setup'
+
+require 'open-uri'
+require 'mini_exiftool'
+
+# Fetch an external photo
+filename = open('/tmp/img_08.jpg').path
+
+# Read the metadata
+photo = MiniExiftool.new filename
+
+
+
+# Print the metadata
+photo.tags.compact.sort.each do |tag|
+  #puts "#{tag}: #{photo[tag]}"
+  puts tag.ljust(28) + photo[tag].to_s
+end
