ead208a3b5038df4c704353174d67051e59b1323
Mihály Nyeste <mihaly.nyeste@gmail.com>
2013-10-20 21:54:38 +0200

    Determine path

--- lib/dpo/exif_data.rb
+++ lib/dpo/exif_data.rb
@@ -4,13 +4,16 @@ module Dpo
 
   class ExifData
 
-    attr_reader :filename
+    attr_reader :filename, :dstpath
 
     def initialize(filename)
       photo = MiniExiftool.new filename
       
       @filename = photo['CreateDate'].strftime('%Y%m%d_%H%M%S')
-    
+      
+      @dstpath = Pathname.new(photo['CreateDate'].strftime('%Y'))  
+      @dstpath += photo['CreateDate'].strftime('%F')
+
     end
 
 
