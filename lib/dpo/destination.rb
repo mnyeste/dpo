6bf6eec41eff8831b1e8d45258736b95dd48eae0
Mihály Nyeste <mihaly.nyeste@gmail.com>
2013-12-28 00:03:46 +0100

    destination is a class

--- lib/dpo/destination.rb
+++ lib/dpo/destination.rb
@@ -7,28 +7,31 @@ module Dpo
     EXIF_TAG_DTO = 'DateTimeOriginal'
     EXIF_TAG_SSTO = 'SubSecTimeOriginal'
 
-    def self.folder(exif_data)
-      dto = exif_data[EXIF_TAG_DTO]
+    def initialize(exif_data, extension)
 
-      raise RuntimeError.new("EXIF tag [#{EXIF_TAG_DTO}] does not exist") unless dto
+      @dto = exif_data[EXIF_TAG_DTO]
+      raise RuntimeError.new("EXIF tag [#{EXIF_TAG_DTO}] does not exist") unless @dto
 
-      dstpath = Pathname.new dto.strftime('%Y')
-      dstpath += dto.strftime('%F')
+      @ssto = exif_data[EXIF_TAG_SSTO]
+      @extension = extension
+
     end
 
-    def self.file(exif_data, extension)
-      dto = exif_data[EXIF_TAG_DTO]
-      raise RuntimeError.new("EXIF tag [#{EXIF_TAG_DTO}] does not exist") unless dto
+    def folder
 
-      filename = dto.strftime('%Y%m%d_%H%M%S')
+      dstpath = Pathname.new @dto.strftime('%Y')
+      dstpath += @dto.strftime('%F')
+    end
 
-      ssto = exif_data[EXIF_TAG_SSTO]
+    def file 
 
-      if ssto 
-        filename += ('_' + ssto.to_s)
+      filename = @dto.strftime('%Y%m%d_%H%M%S')
+
+      if @ssto 
+        filename += ('_' + @ssto.to_s)
       end
 
-      filename += extension
+      filename += @extension
     end 
 
   end
--- test/destination_test.rb
+++ test/destination_test.rb
@@ -9,11 +9,10 @@ class DestinationTest < MiniTest::Unit::TestCase
       Dpo::Destination::EXIF_TAG_DTO => Time.new(2013,4,18,5,5,10)
     }
 
-    folder = Dpo::Destination.folder(stub_exif_data)
-    file = Dpo::Destination.file(stub_exif_data, '.jpg')
+    dest = Dpo::Destination.new(stub_exif_data, '.jpg') 
 
-    assert_equal(Pathname.new('2013/2013-04-18'), folder)
-    assert_equal('20130418_050510.jpg', file)
+    assert_equal(Pathname.new('2013/2013-04-18'), dest.folder)
+    assert_equal('20130418_050510.jpg', dest.file)
   end
 
   def test_with_subsecs
@@ -23,17 +22,16 @@ class DestinationTest < MiniTest::Unit::TestCase
       Dpo::Destination::EXIF_TAG_SSTO => 40
     }
 
-    folder = Dpo::Destination.folder(stub_exif_data)
-    file = Dpo::Destination.file(stub_exif_data, '.jpg')
+    dest = Dpo::Destination.new(stub_exif_data, '.jpg') 
     
-    assert_equal(Pathname.new('2013/2013-11-18'), folder)
-    assert_equal('20131118_083015_40.jpg', file)
+    assert_equal(Pathname.new('2013/2013-11-18'), dest.folder)
+    assert_equal('20131118_083015_40.jpg', dest.file)
   end  
 
   def test_no_exif
 
     assert_raises(RuntimeError) {
-      dst = Dpo::Destination.folder(Hash.new)
+      dest = Dpo::Destination.new(Hash.new,'.jpg')
     }
     
   end  
