e671836c7cfd350a1e81117b5c951004798a1e9f
Mihály Nyeste <mihaly.nyeste@gmail.com>
2014-01-04 10:49:28 +0100

    progress with copy

--- spec/dpo_copy_spec.rb
+++ spec/dpo_copy_spec.rb
@@ -10,8 +10,14 @@ describe Dpo do
     @bin = Pathname.new(__FILE__).parent.parent + "bin"
   end
   
-  it "execute copy" do
-    system("ruby", "#{@bin}/dpo_copy")  
+  it "copy without arguments should fail" do
+    status=system("ruby", "#{@bin}/dpo_copy")
+    assert(!status)
+  end
+
+  it "copy without non-existing folders" do
+    status=system("ruby", "#{@bin}/dpo_copy", "fake1", "fake2")
+    assert(!status)
   end
 
 end
