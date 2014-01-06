8a816b6159b6dbca5de8b50cb2bfc2882d253949
Mihály Nyeste <mihaly.nyeste@gmail.com>
2014-01-06 22:05:28 +0100

    check error conditions

--- spec/dpo_copy_spec.rb
+++ spec/dpo_copy_spec.rb
@@ -15,17 +15,30 @@ describe Dpo do
     assert(!status)
   end
 
-  it "copy without non-existing source" do
+  it "copy from non-existing source should fail" do
     status=system("ruby", "#{@bin}/dpo_copy", "fake1", "fake2")
     assert(!status)
   end
 
-  it "copy without non-existing destination" do
+  it "copy to non-existing destination should fail" do
     Dir.mktmpdir {|dir|      
       tmpdir = Pathname.new(dir)
       srcdir = tmpdir + 'source'
-      FileUtils.mkdir_p(srcdir)
+      FileUtils.mkdir(srcdir)
       
+      status=system("ruby", "#{@bin}/dpo_copy", srcdir.to_s , "fake2")
+      assert(!status)
+    }
+  end
+
+  it "copy to non-writeable destination should fail" do
+    Dir.mktmpdir {|dir|      
+      tmpdir = Pathname.new(dir)
+      srcdir = tmpdir + 'source'
+      FileUtils.mkdir(srcdir)
+      dstdir = tmpdir + 'destination'
+      FileUtils.mkdir(dstdir, :mode => 444)
+
       status=system("ruby", "#{@bin}/dpo_copy", srcdir.to_s , "fake2")
       assert(!status)
     }
