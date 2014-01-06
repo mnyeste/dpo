2dc924e2890e90f0b55ee4301228043acf7a82c0
Mihály Nyeste <mihaly.nyeste@gmail.com>
2014-01-06 21:49:29 +0100

    continue copy

--- spec/dpo_copy_spec.rb
+++ spec/dpo_copy_spec.rb
@@ -15,9 +15,20 @@ describe Dpo do
     assert(!status)
   end
 
-  it "copy without non-existing folders" do
+  it "copy without non-existing source" do
     status=system("ruby", "#{@bin}/dpo_copy", "fake1", "fake2")
     assert(!status)
+  end
+
+  it "copy without non-existing destination" do
+    Dir.mktmpdir {|dir|      
+      tmpdir = Pathname.new(dir)
+      srcdir = tmpdir + 'source'
+      FileUtils.mkdir_p(srcdir)
+      
+      status=system("ruby", "#{@bin}/dpo_copy", srcdir.to_s , "fake2")
+      assert(!status)
+    }
   end
 
 end
