4ac22071cf73ea4c78a47c34d80f803f1b74014d
Mihály Nyeste <mihaly.nyeste@gmail.com>
2014-01-25 23:16:38 +0100

    minor refactor

--- lib/dpo/fingerprint.rb
+++ lib/dpo/fingerprint.rb
@@ -3,16 +3,16 @@ module Dpo
   class Fingerprint
 
     attr_reader :camera, :extension
-    
+
     @@fingerprints = {
       { "Model"=>"NIKON D3200",
         "FileType"=>"JPEG",
-      } => { "camera"=>'nikon-d3200',
-             "extension"=>'jpg'},
-    { "Model"=>"Canon PowerShot S5 IS",
-      "FileType"=>"JPEG",
-      } => { "camera"=>'canon-s5',
-             "extension"=>'jpg'} 
+      } => { :camera=>'nikon-d3200',
+             :extension=>'jpg'},
+      { "Model"=>"Canon PowerShot S5 IS",
+        "FileType"=>"JPEG",
+      } => { :camera=>'canon-s5',
+             :extension=>'jpg'}
     }
 
 
@@ -20,8 +20,8 @@ module Dpo
 
       @@fingerprints.each{|pattern, result|
         if (pattern.to_a & exif_data.to_a).size == pattern.size
-          @camera = result["camera"]
-          @extension = result["extension"]
+          @camera = result[:camera]
+          @extension = result[:extension]
         end
       }
 
