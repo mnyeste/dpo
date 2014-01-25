de9f17f697b5fa06a152a91ec892e53ced805d2a
Mihály Nyeste <mihaly.nyeste@gmail.com>
2014-01-25 22:54:04 +0100

    fingerprint is object

--- lib/dpo/fingerprint.rb
+++ lib/dpo/fingerprint.rb
@@ -5,17 +5,23 @@ module Dpo
     @@fingerprints = {
       { "Model"=>"NIKON D3200",
         "FileType"=>"JPEG",
-      } => 'nikon-d3200',
-      { "Model"=>"Canon PowerShot S5 IS" } => 'canon-s5' 
+        } => 'nikon-d3200',
+      { "Model"=>"Canon PowerShot S5 IS" } => 'canon-s5'
     }
 
-    def self.camera(exif_data)
-      
-      @@fingerprints.each{|fingerprint, model| 
-          return model if (fingerprint.to_a & exif_data.to_a).any? 
-      }
 
+
+    def initialize(exif_data)
+      @exif_data = exif_data
     end
+
+    def camera
+      @@fingerprints.each{|fingerprint, model|
+        return model if (fingerprint.to_a & @exif_data.to_a).size == fingerprint.size
+      }
+      return nil
+    end
+
 
   end
 
--- test/fingerprint_test.rb
+++ test/fingerprint_test.rb
@@ -1,11 +1,33 @@
 require "test_helper"
 
-class CameraTest < MiniTest::Unit::TestCase
+class FingerprintTest < MiniTest::Unit::TestCase
+  
+  def test_fingerprint_match
 
-  def test_fingerprint_match 
+    stub_exif = {
+      "MIMEType"=>"image/jpeg",
+      "JFIFVersion"=>1.01,
+      "FileType"=>"JPEG",
+      "Model"=>"Canon PowerShot S5 IS"
+    }
 
-    assert_equal("canon-s5", Dpo::Fingerprint.camera({"FileType"=>"JPEG", "Model"=>"Canon PowerShot S5 IS"}))
+    f = Dpo::Fingerprint.new(stub_exif)
 
+    assert_equal("canon-s5", f.camera)
   end
 
+  def test_partial_fingerprint_doesnt_match
+    f = Dpo::Fingerprint.new({"Model"=>"NIKON D3200"})
+    assert_nil(f.camera)
+  end
+
+  def test_fake_exif_doesnt_match
+    f = Dpo::Fingerprint.new({"ABC"=>"NIKON D3200"})
+    assert_nil(f.camera)
+  end
+
+  def test_empty_exif_doesnt_match
+    f = Dpo::Fingerprint.new({})
+    assert_nil(f.camera)
+  end
 end
